7e224fb25cd1d000cbc611e7afffa9fef92c8eaa
Padraig MacGabann <47158074+padraigUCSD@users.noreply.github.com>
2021-02-08 05:37:34 -0800

    try something else yet again

--- server.js
+++ server.js
@@ -1,21 +1,13 @@
 // server.js
-var http = require('http');
-var url = require('url);
-var fs = require('fs');
+const http = require('http')
+const fs = require('fs')
 
-http.createServer(function (req, res) {
-  var q = url.parse(req.url, true);
-  var filename = "." + q.pathname;
-  fs.readFile(filename, function(err, data) {
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
-    } 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(8080);
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'content-type': 'text/html' })
+  fs.createReadStream('index.html').pipe(res)
+})
+
+server.listen(process.env.PORT || 3000)
 
  /*
 // Create an instance of the http server to handle HTTP requests
