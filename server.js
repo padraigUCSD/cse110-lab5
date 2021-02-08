eacd2c1076e33d1daf87ea881be7810ca4ca47e6
Padraig MacGabann <47158074+padraigUCSD@users.noreply.github.com>
2021-02-08 05:34:48 -0800

    try another setup for server.js

--- server.js
+++ server.js
@@ -1,27 +1,36 @@
 // server.js
-const http = require('http');
-let fs = require('fs');
+var http = require('http');
+var url = require('url);
+var fs = require('fs');
 
+http.createServer(function (req, res) {
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    } 
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
+
+ /*
 // Create an instance of the http server to handle HTTP requests
 let app = http.createServer((req, res) => {
     // Set a response type of plain text for the response
     res.writeHead(200, {'Content-Type': 'text/html'});
     
-    // Attaching a file to the response and sending it back to the browser
-    fs.readFile('./index.html', null, function (error, data) {
-        if (error) {
-            response.writeHead(404);
-            respone.write('Whoops! File not found!');
-        } else {
-            response.write(data);
-        }
-        response.end();
-    });
-    
     // Send back a response and end the connection
     res.end('Hello World! I am a server.js message\n');
 });
 
+*/
+
+/*
 // Start the server on port 3000
 app.listen(process.env.PORT || 3000);
 console.log('Node server running on port 3000');
+*/
