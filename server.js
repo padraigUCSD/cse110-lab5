f71c9a6e7660a79df7f67f3a2009f24364506f97
Padraig MacGabann <47158074+padraigUCSD@users.noreply.github.com>
2021-02-08 05:20:40 -0800

    configure to render html

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const http = require('http');
 // Create an instance of the http server to handle HTTP requests
 let app = http.createServer((req, res) => {
     // Set a response type of plain text for the response
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.writeHead(200, {'Content-Type': 'text/html'});
 
     // Send back a response and end the connection
     res.end('Hello World! I am a server.js message\n');
