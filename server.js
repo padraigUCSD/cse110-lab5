65b4b69ac342d05d9f42a7fe5c0814031cc2b7d8
Padraig MacGabann <47158074+padraigUCSD@users.noreply.github.com>
2021-02-08 05:26:38 -0800

    try and serve index

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
-// Server.js
-
+// server.js
 const http = require('http');
+let fs = require('fs');
 
 // Create an instance of the http server to handle HTTP requests
 let app = http.createServer((req, res) => {
     // Set a response type of plain text for the response
     res.writeHead(200, {'Content-Type': 'text/html'});
-
+    
+    // Attaching a file to the response and sending it back to the browser
+    fs.readFile('./index.html', null, function (error, data) {
+        if (error) {
+            response.writeHead(404);
+            respone.write('Whoops! File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+    
     // Send back a response and end the connection
     res.end('Hello World! I am a server.js message\n');
 });
