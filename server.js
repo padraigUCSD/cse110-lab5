e86ff1e0a57a07b39c4b41e335c592bf597cd896
Padraig MacGabann <47158074+padraigUCSD@users.noreply.github.com>
2021-02-08 05:12:23 -0800

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ let app = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
 
     // Send back a response and end the connection
-    res.end('Hello World!\n');
+    res.end('Hello World! I am a server.js message\n');
 });
 
 // Start the server on port 3000
